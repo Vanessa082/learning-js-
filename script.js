f52349a797c21fae14f2f89bdb4d337f53c88775
vanessa082 <wahvanessa22@gmail.com>
2024-01-20 20:05:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+// this code works the modern way
+
+// const popup = 
+//     alert ('hello world')
+
+//     let message = "This";
+
+// // repeated 'let' leads to an error
+// let message = "That"; // SyntaxError: 'message' has already been declared
+
+// importance: 2
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin );
